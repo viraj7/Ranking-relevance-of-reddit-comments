ec8a7525ea67bf03ae29268e98693edec235d0cf
Viraj Jagale <viraj25j@gmail.com>
2019-03-07 14:30:31 -0500

    auth file

--- get_data.py
+++ get_data.py
@@ -1,13 +1,17 @@
 from json import dumps
 from collections import defaultdict
-import praw
-reddit = praw.Reddit(client_id='zneTpAmTMY7CoQ', client_secret='McixCrruKKChhHw3SElyvVleb5w', user_agent='python nlp project by /u/viraj25j')
+import praw, configparser
+
+config = configparser.ConfigParser()
+config.read('oauth.conf')
+client_key = config['keys']['client_id']
+secret_key = config['keys']['client_secret']
+reddit = praw.Reddit(client_id=client_key, client_secret=secret_key, user_agent='python nlp project by /u/viraj25j')
 data_file = open("data.json", "w")
 text = defaultdict(list)
 for post in reddit.subreddit('ucf').top(limit=200):
     post.comments.replace_more(limit=32, threshold=0)
     for comment in post.comments.list():
-        text[post.title].append(comment.body)
-print(len(text))        
+        text[post.title].append(comment.body)      
 data_file.write(dumps(text))
 data_file.close()
